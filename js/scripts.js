5627f03d12804bdefd6bd99d3c44592797449d33
Parley <parley@parleysthinkpad.attlocal.net>
2016-10-04 14:31:30 -0700

    first

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+jQuery(function(){
+
+    jQuery('#0').click(function(){
+        jQuery('#overlay1').toggle();
+    });
+    jQuery('#1').click(function(){
+        jQuery('#overlay2').toggle();
+    });
+    jQuery('#2').click(function(){
+        jQuery('#overlay3').toggle();
+    });
+    jQuery('#3').click(function(){
+        jQuery('#overlay4').toggle();
+    });
+
+       });
