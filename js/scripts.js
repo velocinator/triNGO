6f8456d5a0afa094dc04d49f34efc4a4ea853a2b
Connor22 <connors.chunk@gmail.com>
2016-10-04 17:42:16 -0400

    Overlays are now the buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,5 @@
 jQuery(function(){
-
-    jQuery('#0').click(function(){
-        jQuery('#overlay1').toggle();
+    jQuery('.overlay').click(function(){
+        $(this).toggle();
     });
-    jQuery('#1').click(function(){
-        jQuery('#overlay2').toggle();
-    });
-    jQuery('#2').click(function(){
-        jQuery('#overlay3').toggle();
-    });
-    jQuery('#3').click(function(){
-        jQuery('#overlay4').toggle();
-    });
-
-       });
+});
