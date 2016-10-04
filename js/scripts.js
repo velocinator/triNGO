7143881a0d2027b9594db6558dc603ae05cb9e8a
Connor22 <connors.chunk@gmail.com>
2016-10-04 18:47:04 -0400

    Added custom image setting
    Increased size

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(function(){
     $('.overlay').click(function(){
-        $(this).toggle();
+        $(this).fadeOut();
+    });
+    $('#submit').click(function(){
+        var hidden = $('#url').val();
+        $('#hiddenimg').attr("src", hidden);
+        $('.initial').toggle();
+        $('#image').toggle();
+        //$('body').css('background-color', 'fuschia')
     });
 });
+
